747015d882994b71d23b4db66b758230ccefffc0
Nicola <39983382+nisol91@users.noreply.github.com>
2020-07-19 16:05:27 +0200

    add script

--- src/app/models/todo.model.ts
+++ src/app/models/todo.model.ts
@@ -1,8 +1,10 @@
 import { model, models, Schema } from "mongoose";
 
 const todoSchema: Schema = new Schema({
-  required: true,
-  type: String, // String with a capital letter
+  text: {
+    required: true,
+    type: String, // String with a capital letter
+  },
 });
 
 export const Todo = models.Todo || model("Todo", todoSchema);
