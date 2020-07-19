3aab6a6e9c03f0b95d4d68df4868737a9a4e10de
Nicola <39983382+nisol91@users.noreply.github.com>
2020-07-19 16:22:55 +0200

    api and tests

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,9 +1,13 @@
-import { controller } from '@foal/core';
+import { controller, Log } from "@foal/core";
 
-import { ApiController } from './controllers';
+import { ApiController } from "./controllers";
 
+@Log("----- AppController ----", {
+  body: true,
+  headers: ["X-CSRF-Token"],
+  params: true,
+  query: true,
+})
 export class AppController {
-  subControllers = [
-    controller('/api', ApiController),
-  ];
+  subControllers = [controller("/api", ApiController)];
 }
